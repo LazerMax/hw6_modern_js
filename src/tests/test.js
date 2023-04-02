eaaa7b5de9af9f3ec9bd7ee55d527c02969a14f9
Maksim Lazarev <felshines@gmail.com>
2023-04-02 13:17:38 +0300

    create template

--- src/tests/test.js
+++ src/tests/test.js
@@ -1,4 +1,6 @@
-
-test("testing orderByPropsFunction"){
-
-}
+test('testing Character constructor function', () => {
+  const obj = {
+    name: 'мечник', health: 10, level: 2, attack: 80, defence: 40,
+  };
+  expect(obj).toEqual(obj);
+});
